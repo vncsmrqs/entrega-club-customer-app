0db72d74b89eeeec45fef4ac132939d3acf6f6f5
Vinicius Marques <vncsmrqs@gmail.com>
2023-08-05 02:28:42 -0300

    fix: pull to reload

--- src/components/Screen/ScreenMain.vue
+++ src/components/Screen/ScreenMain.vue
@@ -28,15 +28,19 @@
   const touchY = ref(0);
   const touchDiff = ref(0);
   const scrollTop = ref(0);
+  const startTop = ref(0);
 
   const touchstart = (e: any) => {
+    startTop.value = scrollableElement.value?.scrollTop || 0;
     touchstartY.value = e.touches[0].clientY;
   };
 
   const touchmove = (e: any) => {
-    touchY.value = e.touches[0].clientY;
-    touchDiff.value = touchY.value - touchstartY.value;
-    scrollTop.value = scrollableElement.value?.scrollTop || 0;
+    if (startTop.value === 0) {
+      touchY.value = e.touches[0].clientY;
+      scrollTop.value = scrollableElement.value?.scrollTop || 0;
+      touchDiff.value = touchY.value - touchstartY.value;
+    }
   };
 
   const touchend = (e: any) => {
@@ -45,13 +49,14 @@
       e.preventDefault();
       emit('reload');
     }
+    startTop.value = 0;
     touchstartY.value = 0;
     touchY.value = 0;
     touchDiff.value = 0;
   };
 
   const updateIconYPosition = computed(() => {
-    if (touchDiff.value > 0) {
+    if (touchDiff.value > 0 && scrollTop.value === 0) {
       return touchDiff.value > 200 ? 200 : touchDiff.value;
     }
     return 0;
@@ -67,15 +72,22 @@
     @touchmove="touchmove"
     @touchend="touchend"
   >
+    <div class="fixed z-10 bg-red-500 top-0 left-0">
+      <div>start: {{ touchstartY }}</div>
+      <div>move: {{ touchY }}</div>
+      <div>diff: {{ touchDiff }}</div>
+      <div>top: {{ scrollTop }}</div>
+      <div>icon-position {{ updateIconYPosition }}</div>
+    </div>
     <Transition name="fade">
       <div
         v-show="updateIconYPosition"
         class="w-10 h-10 bg-white drop-shadow rounded-full absolute top-0 left-1/2 z-40 flex items-center justify-center text-primary-600"
         :style="{
           'z-index': 99999,
-          transform: `translateX(-50%) translateY(calc(${updateIconYPosition}px - 100%)) rotate(${
-            updateIconYPosition * 2
-          }deg)`,
+          transform: `translateX(-50%) translateY(calc(${
+            updateIconYPosition / 2
+          }px - 100%)) rotate(${updateIconYPosition * 2}deg)`,
         }"
       >
         <ReloadIcon :style="{ opacity: updateIconYPosition / 2 / 100 }" />
